3307908700f7e2c5809acade26b21b574bf46161
Artem Silka <silkartem@gmail.com>
2020-09-24 23:43:14 -0400

    initial

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
+// @refresh reset
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase'
+import 'firebase/firestore'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAzwa9jPHSYKdaLFDK0zJKa5y1kkb3kuaw",
+  authDomain: "ays-react-native-messanger.firebaseapp.com",
+  databaseURL: "https://ays-react-native-messanger.firebaseio.com",
+  projectId: "ays-react-native-messanger",
+  storageBucket: "ays-react-native-messanger.appspot.com",
+  messagingSenderId: "481340587603",
+  appId: "1:481340587603:web:3d73946e7068d02482d0a6",
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
   return (
